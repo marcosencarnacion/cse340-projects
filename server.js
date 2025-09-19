e5165dd5fac39d8f859b9c42e4123409bc08d608
marcosencarnacion <enc24001@byui.edu>
2025-09-19 18:11:26 -0500

    Completed Error Handling Activity

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
+const utilities = require("./utilities")
 
 // Added inventory route require statement
 const inventoryRoute = require("./routes/inventoryRoute")
@@ -30,6 +31,25 @@ app.use(static)
 app.get("/", baseController.buildHome)
 // Inventory routes - Added
 app.use("/inv", inventoryRoute)
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
+
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message: err.message,
+    nav
+  })
+})
 
 
 /* ***********************
