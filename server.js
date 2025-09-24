f2922818dfa0caaee59791c0f1de0be113b2c2d8
marcosencarnacion <enc24001@byui.edu>
2025-09-24 08:14:07 -0500

    Completed Activity Instruction 1: Sessions and Messages

--- server.js
+++ server.js
@@ -12,9 +12,35 @@ const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
 const utilities = require("./utilities")
+const session = require("express-session")
+const pool = require('./database/')
+
 
 // Added inventory route require statement
 const inventoryRoute = require("./routes/inventoryRoute")
+
+
+/* ***********************
+ * Middleware
+ * ************************/
+ app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
+
 
 /* ***********************
  * View Engine and Templates
